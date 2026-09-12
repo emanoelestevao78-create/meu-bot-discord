c90b1215a497e248bc56aae52c02b82d1cdab485
emanoelestevao78-create <emanoelestevao78@gmail.com>
2026-09-12 20:49:12 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.send('Bot online!'));
+app.listen(process.env.PORT || 3000);
 import express from 'express';
 import "dotenv/config";
 import {
